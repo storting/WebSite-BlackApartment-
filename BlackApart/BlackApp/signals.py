cbf5302f114da7fbbf6d8316d4bb361d7cfbfd98
storting <144237030+storting@users.noreply.github.com>
2026-04-16 09:55:56 +0700

    merge commit temp

--- BlackApart/BlackApp/signals.py
+++ BlackApart/BlackApp/signals.py
@@ -7,12 +7,8 @@ from .models import Property
 
 @receiver(pre_save, sender=Property)
 def geocode_property(sender, instance, **kwargs):
-    print("\n" + "="*50)
-    print(f"🟢 СИГНАЛ pre_save для объекта id={instance.id}, адрес='{instance.address}'")
-    print(f"Текущие координаты: lat={instance.latitude}, lon={instance.longitude}")
     
     if instance.address and (instance.latitude is None or instance.longitude is None):
-        print("🔄 Запускаем геокодирование...")
         try:
             api_key = settings.YANDEX_API_KEY
             geolocator = Yandex(api_key='18d8b773-5924-4a29-8afc-8918512836c9')
@@ -20,11 +16,9 @@ def geocode_property(sender, instance, **kwargs):
             if location:
                 instance.latitude = location.latitude
                 instance.longitude = location.longitude
-                print(f"✅ Координаты обновлены: {instance.latitude}, {instance.longitude}")
             else:
-                print("❌ Геокодер вернул None — адрес не найден.")
+                return
         except Exception as e:
-            print(f"🔥 Ошибка геокодера: {e}")
+            return 
     else:
-        print("⏭️ Пропускаем геокодирование (нет адреса или координаты уже заданы).")
-    print("="*50 + "\n")
+        return
